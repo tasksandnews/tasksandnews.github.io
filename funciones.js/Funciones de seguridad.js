f90c46c2946a58051baf1c2ebb8f84541918b895
tasksandnews <aplanetgsoporte@gmail.com>
2022-01-22 01:38:20 -0800

    update

---
+++ funciones.js/Funciones de seguridad.js
@@ -0,0 +1,23 @@
+function ir(){
+    
+var u="G4001P";
+   if(document.form.login.value==u){
+   alert ("BIENVENIDO, Esta Accion Solo Sera Necesaria Realizarla Una Vez");
+   window.location="Inicio.html";}
+
+   else{
+   alert("Por Nuestra Seguridad Es Necesario Que Igreces El Codigo Correctamente, Intentalo De Nuevo!");
+}
+
+}
+
+function info(){
+
+   alert("Si No Sabes El Codigo Puedes Preguntarle Al Jefe De Grupo o Pedir Informacion En El Grupo Local De Messenger!")
+
+
+}
+
+function how(){
+   alert("Puedes contribuir enviando tus tareas al numero que esta en la informacion MVP de la siguiente manera indicando el orden si son varias imagenes y numero de la tarea, recuerda que puedes pedir que se censure tu nombre  ")
+}
